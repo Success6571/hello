0d2b0f6ad86670d59c2f33be491def83de9c4bd7
Your Name <youremail@huskers.unl.edu>
2025-08-28 10:24:08 -0500

    initial commit

---
+++ hello/hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Alexander S.
+ * Date: 08/28/2025
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Alexander Santos\n");
+
+    return 0;
+}
+
